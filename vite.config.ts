365a09828d6f698913eb830b62f4c946d736b389
詹詹 <11470010+w1120930940@user.noreply.gitee.com>
2024-09-04 18:16:15 +0800

    路径优化

--- vite.config.ts
+++ vite.config.ts
@@ -28,4 +28,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src/'),
     }
   },
+  server: {
+    host: '0.0.0.0', //ip地址
+    port: 8080, //端口号
+    open: true //启动后是否自动打开浏览器
+  }
 })
